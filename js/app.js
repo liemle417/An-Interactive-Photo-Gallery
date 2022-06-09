65a0c21798bf35a0bf070251b20a4754df412f8a
liemle417 <47941056+liemle417@users.noreply.github.com>
2022-06-09 01:04:39 -0700

    Upload Files 6-9-2022

---
+++ js/app.js
@@ -0,0 +1,42 @@
+baguetteBox.run('.gallery');
+//const search = new Filter('search', 'data-caption');
+
+
+$('a[href^="photo"] p').on('mouseover', function(){
+    $('.gallery p').css({ //Object Literal
+        'color': 'white',
+        'background-color': 'black',
+        'font-size': '2em',
+        'font-family': 'san-serif',
+    });
+});
+
+
+
+
+function createSearchFunction() {
+
+    // Declare variables
+
+    let input = document.getElementById('search'); //When a user type, GET all the info from input
+    let inputCapitalize = input.value.toUpperCase(); //Make the info capitalized
+
+    //Get all information from the BIG list: ul --> li --> a. Because the attribute 'data-caption' lives in the a list.
+    let ul = document.getElementById('searchPhoto'); //Get all the ul list
+    let li = ul.getElementsByTagName('li'); //Go deeper, get the li list
+    let a = ul.getElementsByTagName('a'); //Last, get the a list. So, we can access to the attribute 'data-caption'
+
+    
+    // Loop through all list items, and hide those who don't match the search query
+    for (let i = 0; i < a.length; i++) {
+        let dataCaption = a[i].getAttribute('data-caption'); //get dataCaption info
+        let dataCaptionCapitalize = dataCaption.toUpperCase(); //Make it capitalized
+
+        if (dataCaptionCapitalize.indexOf(inputCapitalize) == -1) { //-1 means "nothing is found." indexOf means search the capitalized info from input vs the capitalized dataCaption in the a list.
+            li[i].style.display = "none";
+        } else {
+            li[i].style.display = "";
+        }
+    }
+};
+
